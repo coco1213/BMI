93fe873795128cca9df45f3fd9a3898ec08966dd
m305-64\w401 <a106082025@mail.shu.edu.com>
2018-03-15 18:11:28 +0800

    BMI

---
+++ test 01/test 01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            
+            Console.WriteLine("請輸入身高(公分):");
+            Console.WriteLine("請輸入體重:");
+            string input = Console .ReadLine ();
+            string input1 = Console.ReadLine();
+
+            double s = double .Parse (input1)/(double .Parse(input)/100*double .Parse (input)/100);
+            Console.WriteLine("BMI ="+Math .Round (s));
+            Console.ReadLine();
+
+        }
+    }
+}
